c71ff2274e4d7cba31454b08a12008d09dd11e50
Sergei Mironenko <sergei.mironenko@bolt.eu>
2022-01-14 17:08:25 +0200

    update orgmode config

--- nvim/lua/plugin/orgmode.lua
+++ nvim/lua/plugin/orgmode.lua
@@ -1,5 +1,5 @@
 require('orgmode').setup({
     org_agenda_files = '~/Documents/org/*',
     org_default_notes_file = '~/Documents/org/todo.org',
-    org_hide_leading_stars = true,
+    --org_hide_leading_stars = true,
 })
