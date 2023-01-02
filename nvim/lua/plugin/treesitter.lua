d23dab628ea69572bdeb6ed18229a0f3edaf2761
Sergei Mironenko <sergei.mironenko@bolt.eu>
2023-01-02 15:08:46 +0200

    disable obsolete treesitter config option

--- nvim/lua/plugin/treesitter.lua
+++ nvim/lua/plugin/treesitter.lua
@@ -1,5 +1,4 @@
 local parser_config = require 'nvim-treesitter.parsers'.get_parser_configs()
-parser_config.tsx.used_by = { 'javascript', 'typescript.tsx' }
 parser_config.org = {
     install_info = {
         url = 'https://github.com/milisims/tree-sitter-org',
