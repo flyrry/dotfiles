c85c274d62f54e48cdb7569d9119e3905cea761e
Sergei Mironenko <sergei.mironenko@bolt.eu>
2023-02-10 16:43:34 +0200

    a few plugins to try

--- nvim/lua/plugins/to-try.lua
+++ nvim/lua/plugins/to-try.lua
@@ -1,4 +1,7 @@
 return {
+--  'junegunn/gv.vim',
+--  'folke/todo-comments.nvim',
+--  'folke/persistence.nvim',
 --    'tpope/vim-dispatch',
 --    'sindrets/diffview.nvim',
 --    'folke/trouble.nvim',
