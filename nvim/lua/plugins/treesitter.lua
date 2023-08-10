750a5405f05d6de08cfcf5672dca8ec6dad9ca05
Sergei Mironenko <sergei.mironekno@bolt.eu>
2023-08-10 17:14:38 +0300

    use treesitter navigation

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -49,4 +49,27 @@ return {
             vim.o.foldexpr = 'nvim_treesitter#foldexpr()'
         end
     },
+    {
+        'nvim-treesitter/nvim-treesitter-textobjects',
+        config = function()
+            require 'nvim-treesitter.configs'.setup({
+                textobjects = {
+                    move = {
+                        enable = true,
+                        set_jumps = true,
+                        goto_next_start = {
+                            [']m'] = '@function.outer',
+                            [']]'] = '@class.outer',
+                            [']a'] = '@parameter.inner',
+                        },
+                        goto_previous_start = {
+                            ['[m'] = '@function.outer',
+                            ['[['] = '@class.outer',
+                            ['[a'] = '@parameter.outer',
+                        },
+                    }
+                }
+            })
+        end
+    },
 }
