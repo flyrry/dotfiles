48d8112ccec91b02f6f19a40badf19130958325c
Sergei Mironenko <sergei.mironenko@bolt.eu>
2023-09-13 16:55:18 +0300

    replace surround

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -1,8 +1,12 @@
 return {
     { 'echasnovski/mini.comment',   opts = {} },
     { 'echasnovski/mini.splitjoin', opts = {} },
-    { 'echasnovski/mini.surround',  opts = {} },
     { 'tpope/vim-fugitive' },
     { 'tpope/vim-rhubarb' },
     { 'tpope/vim-sleuth' }, -- auto-adjust 'shiftwidth' and 'expandtab'
+    {
+        "kylechui/nvim-surround",
+        event = "VeryLazy",
+        config = true
+    },
 }
