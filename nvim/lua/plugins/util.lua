6a584758f13c169a33134c83a55ea1e205ce458b
Sergei Mironenko <sergei.mironenko@bolt.eu>
2023-09-13 16:55:03 +0300

    more pending plugins

--- nvim/lua/plugins/util.lua
+++ nvim/lua/plugins/util.lua
@@ -1,50 +1,49 @@
 return {
-    'github/copilot.vim',
-    {
-        'folke/which-key.nvim',
-        config = function()
-            require('which-key').register({
-                ["<C-j>"] = { "<C-w>j" },
-                ["<C-k>"] = { "<C-w>k" },
-                ["<C-h>"] = { "<C-w>h" },
-                ["<C-l>"] = { "<C-w>l" },
+  'github/copilot.vim',
+  {
+    'folke/which-key.nvim',
+    config = function()
+      require('which-key').register({
+        ["<C-j>"] = { "<C-w>j" },
+        ["<C-k>"] = { "<C-w>k" },
+        ["<C-h>"] = { "<C-w>h" },
+        ["<C-l>"] = { "<C-w>l" },
 
-                --j = {'gj'},
-                --k = {'gk'},
-                --S = {'"_diwP', "Replace"},
-                ["<leader>"] = {
-                    --fed = {':e ~/.config/nvim/init.lua<CR>', "Edit nvim config"},
+        --j = {'gj'},
+        --k = {'gk'},
+        --S = {'"_diwP', "Replace"},
+        ["<leader>"] = {
+          --fed = {':e ~/.config/nvim/init.lua<CR>', "Edit nvim config"},
 
-                    ['/'] = { ':nohlsearch<CR>', "Clear highlighted search terms" },
+          ['/'] = { ':nohlsearch<CR>', "Clear highlighted search terms" },
 
-                    --t = {
-                    --l = {':lvim /\\CTODO/ % | lw<CR>', "List TODOs"},
-                    --t = {':lvim /\\CTHEN/ % | lw<CR>', "List THENs"},
-                    --f = {':exec ":lvim /".input("Search string: ")."/ % | lw"<CR>', "Find in buffer"},
-                    --c = {':s /- \\[TODO\\]/+/<CR>', "Mark TODO complete"},
-                    --C = {':s /- \\[TODO\\]/+/<CR>dd/DONE<CR>p', "Mark TODO complete and archive"},
-                    --},
+          --t = {
+          --l = {':lvim /\\CTODO/ % | lw<CR>', "List TODOs"},
+          --t = {':lvim /\\CTHEN/ % | lw<CR>', "List THENs"},
+          --f = {':exec ":lvim /".input("Search string: ")."/ % | lw"<CR>', "Find in buffer"},
+          --c = {':s /- \\[TODO\\]/+/<CR>', "Mark TODO complete"},
+          --C = {':s /- \\[TODO\\]/+/<CR>dd/DONE<CR>p', "Mark TODO complete and archive"},
+          --},
 
-                    --i = {
-                    --n = {'"=strftime("%d.%m.%Y %H:%M:%S")<CR>P', "Insert date and time"},
-                    --d = {'"=strftime("%d.%m.%Y")<CR>P', "Insert date"},
-                    --t = {'"=strftime("%H:%M:%S")<CR>P', "Insert time"},
-                    --},
+          --i = {
+          --n = {'"=strftime("%d.%m.%Y %H:%M:%S")<CR>P', "Insert date and time"},
+          --d = {'"=strftime("%d.%m.%Y")<CR>P', "Insert date"},
+          --t = {'"=strftime("%H:%M:%S")<CR>P', "Insert time"},
+          --},
 
-                    s = { ':split<CR>', "horizontal split" },
-                    v = { ':vsplit<CR>', "vertical split" },
-                },
-            }, { silent = true })
-        end,
-    },
-    {
-        'ggandor/leap.nvim',
-        lazy = true,
-        keys = {
-            { 's',  '<Plug>(leap-forward-to)',   desc = 'Leap forward',  silent = true },
-            { 'S',  '<Plug>(leap-backward-to)',  desc = 'Leap backward', silent = true },
-            { 'gs', '<Plug>(leap-cross-window)', desc = 'Leap across',   silent = true },
+          s = { ':split<CR>', "horizontal split" },
+          v = { ':vsplit<CR>', "vertical split" },
         },
-        opts = {}
+      }, { silent = true })
+    end,
+  },
+  {
+    'ggandor/leap.nvim',
+    config = true,
+    keys = {
+      { 's',  '<Plug>(leap-forward-to)',   desc = 'Leap forward',  silent = true },
+      { 'S',  '<Plug>(leap-backward-to)',  desc = 'Leap backward', silent = true },
+      { 'gs', '<Plug>(leap-cross-window)', desc = 'Leap across',   silent = true },
     },
+  },
 }
