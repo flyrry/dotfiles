3f47a3795ae50d33c4f5ee5f7b3b4d97cb5db47a
Sergei Mironenko <sergei.mironenko@bolt.eu>
2023-09-18 12:19:34 +0300

    improve telescope keybinds

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -24,6 +24,38 @@ return {
             })
         end,
         config = function()
+            require('telescope').setup({
+                defaults = {
+                    mappings = {
+                        i = {
+                            ['<Down>'] = require('telescope.actions').move_selection_next,
+                            ['<C-j>'] = require('telescope.actions').move_selection_next,
+                            ['<C-n>'] = require('telescope.actions').move_selection_next,
+
+                            ['<Up>'] = require('telescope.actions').move_selection_previous,
+                            ['<C-k>'] = require('telescope.actions').move_selection_previous,
+                            ['<C-p>'] = require('telescope.actions').move_selection_previous,
+
+                            ['<C-f>'] = require('telescope.actions').smart_send_to_qflist + require('telescope.actions').open_qflist,
+                            ['<C-l>'] = require('telescope.actions').smart_send_to_loclist + require('telescope.actions').open_loclist,
+                        },
+                        n = {
+                            ['<Down>'] = require('telescope.actions').move_selection_next,
+                            ['j'] = require('telescope.actions').move_selection_next,
+                            ['n'] = require('telescope.actions').move_selection_next,
+
+                            ['<Up>'] = require('telescope.actions').move_selection_previous,
+                            ['k'] = require('telescope.actions').move_selection_previous,
+                            ['p'] = require('telescope.actions').move_selection_previous,
+
+                            ['<C-f>'] = require('telescope.actions').smart_send_to_qflist + require('telescope.actions').open_qflist,
+                            ['<C-l>'] = require('telescope.actions').smart_send_to_loclist + require('telescope.actions').open_loclist,
+                        },
+                    },
+                    dynamic_preview_title = true,
+                }
+            })
+
             require('telescope').load_extension('fzf')
             require('telescope').load_extension('dir')
         end,
