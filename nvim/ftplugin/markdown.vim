ba72c5bc93bbc17387f784cc20aaa2a6468daf16
Sergei Mironenko <sergei.mironenko@bolt.eu>
2023-10-04 10:55:43 +0300

    enable conceal and folding for markdown globally

---
+++ nvim/ftplugin/markdown.vim
@@ -0,0 +1,5 @@
+let b:undo_ftplugin = "conceallevel<"
+
+if has("conceal")
+    setlocal conceallevel=2
+endif
--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -49,3 +49,5 @@ vim.keymap.set('n', '<C-h>', '<C-w>h')
 vim.keymap.set('n', '<C-j>', '<C-w>j')
 vim.keymap.set('n', '<C-k>', '<C-w>k')
 vim.keymap.set('n', '<C-l>', '<C-w>l')
+
+vim.g.markdown_folding = 1
