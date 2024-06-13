2fed1925b1e2e92e965d140f6385a54baaa4274d
Sergei Mironenko <sergei.mironenko@bolt.eu>
2024-06-13 15:41:37 +0300

    add fish configs

---
+++ fish/config.fish
@@ -0,0 +1,49 @@
+set -gx EDITOR nvim
+set -gx MANPAGER "sh -c 'col -bx | bat --theme \"Monokai Extended\" -l man -p'"
+
+set PATH /opt/homebrew/bin $PATH
+set PATH $HOME/.cargo/bin $PATH
+set PATH $HOME/.local/bin $PATH
+
+export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude ".git"'
+
+fzf --fish | source
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    alias gl="GITHUB_TOKEN=(security find-generic-password -s hub -a sergei -w) gh"
+    alias e=nvim
+    alias jj="yarn jenkins-job"
+    # alias s="kitty +kitten ssh"
+
+    # abbr -a e nvim
+    # abbr -a jj 'yarn jenkins-job'
+    abbr -a s 'kitty +kitten ssh'
+    abbr -a dotdot --regex '^\\.\\.+$' --function multicd
+    abbr -a jira-issue --regex '[A-Z]{3,5}-[0-9]{1,6}' --function openjira
+
+    abbr -a gam 'git commit --amend --reuse-message=HEAD'
+    abbr -a gp 'git pull'
+    abbr -a gpy 'git pull && yarn install'
+    abbr -a gpp 'git pull && pnpm install'
+    abbr -a gpf 'git push --force-with-lease'
+    abbr -a gr 'git fetch origin develop:develop && git rebase develop'
+    abbr -a gra 'git rebase --abort'
+    abbr -a grc 'git rebase --continue'
+    abbr -a gs 'git status --short'
+    abbr -a gco 'git checkout'
+    abbr -a gcd 'git checkout develop'
+    abbr -a gcob 'git checkout -b'
+    abbr -a gcoc 'git coc'
+    abbr -a gcm --set-cursor 'git commit -m "%"'
+    abbr -a gsc 'git sco'
+end
+
+#starship init fish | source
+
+#nvm use default --silent
+
+# pnpm
+set -gx PNPM_HOME "/Users/sergei/Library/pnpm"
+set -gx PATH "$PNPM_HOME" $PATH
+# pnpm end
---
+++ fish/functions/openjira.fish
@@ -0,0 +1,3 @@
+function openjira
+  echo open 'http://taxify.atlassian.net/browse/'$argv
+end
