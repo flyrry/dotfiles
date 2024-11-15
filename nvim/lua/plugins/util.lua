01ee7b0cd053e3a96a237e4f67d86f5c5561a80b
Sergei Mironenko <sergei.mironenko@bolt.eu>
2024-11-15 14:18:17 +0200

    replace leap with flash.nvim

--- nvim/lua/plugins/util.lua
+++ nvim/lua/plugins/util.lua
@@ -16,12 +16,19 @@ return {
     end,
   },
   {
-    'ggandor/leap.nvim',
-    config = true,
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    opts = {
+      modes = {
+        char = { enabled = false }
+      }
+    },
     keys = {
-      { 's',  '<Plug>(leap-forward-to)',   desc = 'Leap forward',  silent = true },
-      { 'S',  '<Plug>(leap-backward-to)',  desc = 'Leap backward', silent = true },
-      { 'gs', '<Plug>(leap-cross-window)', desc = 'Leap across',   silent = true },
+      { "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
+      { "S",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
+      { "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
+      { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      { "<C-g>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
     },
   },
   'christoomey/vim-tmux-navigator',
