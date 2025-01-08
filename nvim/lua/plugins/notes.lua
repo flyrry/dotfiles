276bf59088ed0417daaa90b0ea552976d1b99e22
Sergei Mironenko <sergei.mironenko@bolt.eu>
2025-01-08 23:26:26 +0200

    remove unused

--- nvim/lua/plugins/notes.lua
+++ nvim/lua/plugins/notes.lua
@@ -24,24 +24,4 @@ return {
             end
         }
     },
-    {
-        enabled = false,
-        'nvim-neorg/neorg',
-        build = ':Neorg sync-parsers',
-        dependencies = { 'nvim-lua/plenary.nvim' },
-        opts = {
-            load = {
-                ['core.defaults'] = {},
-                ['core.concealer'] = {},
-                ['core.dirman'] = {
-                    config = {
-                        workspaces = {
-                            notes = '~/Documents/notes',
-                            default_workspace = 'notes',
-                        }
-                    }
-                }
-            }
-        },
-    }
 }
