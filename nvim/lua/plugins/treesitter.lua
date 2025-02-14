2e93de96329681a3fe52efb0565b1800387a8073
Sergei Mironenko <sergei.mironenko@bolt.eu>
2025-02-14 13:32:46 +0200

    drop treesitter objects

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,59 +1,31 @@
 return {
     {
         'nvim-treesitter/nvim-treesitter',
-        dependencies = {
-            'nvim-treesitter/nvim-treesitter-textobjects',
-        },
         build = ':TSUpdate',
-        config = function()
-            require 'nvim-treesitter.configs'.setup({
-                highlight = { enable = true },
-                indent = { enable = true },
-                textobjects = { enable = true },
-                ensure_installed = {
-                    'bash',
-                    'cpp',
-                    'fish',
-                    'javascript',
-                    'json',
-                    'lua',
-                    'luadoc',
-                    'markdown',
-                    'python',
-                    'regex',
-                    'rust',
-                    'tsx',
-                    'typescript',
-                    'vim',
-                    'vimdoc',
-                },
-            })
-
-            vim.o.foldmethod = 'expr'
-            vim.o.foldexpr = 'nvim_treesitter#foldexpr()'
-        end
-    },
-    {
-        'nvim-treesitter/nvim-treesitter-textobjects',
-        config = function()
-            require 'nvim-treesitter.configs'.setup({
-                textobjects = {
-                    move = {
-                        enable = true,
-                        set_jumps = true,
-                        goto_next_start = {
-                            [']m'] = '@function.outer',
-                            [']]'] = '@class.outer',
-                            [']a'] = '@parameter.inner',
-                        },
-                        goto_previous_start = {
-                            ['[m'] = '@function.outer',
-                            ['[['] = '@class.outer',
-                            ['[a'] = '@parameter.outer',
-                        },
-                    }
-                }
-            })
-        end
+        main = 'nvim-treesitter.configs',
+        opts = {
+            ensure_installed = {
+                'bash',
+                'cpp',
+                'diff',
+                'fish',
+                'html',
+                'javascript',
+                'json',
+                'lua',
+                'luadoc',
+                'markdown',
+                'regex',
+                'rust',
+                'tsx',
+                'typescript',
+                'query',
+                'vim',
+                'vimdoc',
+            },
+            auto_install = true,
+            highlight = { enable = true },
+            indent = { enable = true },
+        }
     },
 }
