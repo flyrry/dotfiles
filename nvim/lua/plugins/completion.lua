6e88cf147f0f13bf8ef7462e12858e33169d4a49
Sergei Mironenko <sergei.mironenko@bolt.eu>
2025-02-21 14:42:32 +0200

    fix blink

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -42,8 +42,10 @@ return {
             -- elsewhere in your config, without redefining it, due to `opts_extend`
             sources = {
                 default = { 'lsp', 'path', 'snippets', 'buffer' },
-                cmdline = {}
             },
+            cmdline = {
+                sources = {}
+            }
         },
         opts_extend = { "sources.default" }
     }
