c45fcd97f3c721d0e71434fbf27d83cf35aeb44e
Sergei Mironenko <sergei.mironenko@bolt.eu>
2025-06-06 23:15:14 +0300

    update bolt-server config

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -31,10 +31,11 @@ return {
             'nvim-telescope/telescope.nvim',
             'folke/which-key.nvim',
         },
-        cond = function()
-            local cwd = vim.fn.getcwd()
-            return cwd == "/Users/sergei/repos/taxify/server"
-        end,
+        -- commit = '3d9848f',
+        -- cond = function()
+        --     local cwd = vim.fn.getcwd()
+        --     return cwd == "/Users/sergei/repos/taxify/server"
+        -- end,
         config = function()
             local bolt = require("bolt-server")
             require("which-key").add({
@@ -47,12 +48,38 @@ return {
                 { "<Leader>csf", bolt.compile_service_force,      desc = "[C]ompile [S]ervice ([F]orce)" },
                 { "<Leader>cst", bolt.compile_service_with_tests, desc = "[C]ompile [S]ervice with [T]ests" },
                 { "<Leader>ags", bolt.api_gen_service,            desc = "[A]PI-[G]en [S]ervice" },
-                { "<Leader>cts", bolt.select_comp_test_to_run,    desc = "" },
-                { "<Leader>cta", bolt.run_service_comp_tests,     desc = "" },
-                { "<Leader>ctb", bolt.run_buffer_comp_tests,      desc = "" },
-                { "<Leader>ctc", bolt.run_comp_test_under_cursor, desc = "" },
+                { "<Leader>xs",  bolt.select_comp_test_to_run,    desc = "E[x]ecute [S]elected test" },
+                { "<Leader>xa",  bolt.run_service_comp_tests,     desc = "E[x]ecute all service tests" },
+                { "<Leader>xf",  bolt.run_buffer_comp_tests,      desc = "E[x]ecute tests in the file" },
+                { "<Leader>xx",  bolt.run_comp_test_under_cursor, desc = "E[x]ecute current test" },
                 { "<Leader>gad", bolt.goto_api_definition,        desc = "" },
                 { "<Leader>gae", bolt.goto_api_endpoint,          desc = "" },
+            })
+            local bottomsplit = {
+                config = function()
+                    return {
+                        style = 'minimal',
+                        split = 'below',
+                        height = 20,
+                    }
+                end,
+                should_enter = false,
+            }
+            local rightsplit = {
+                config = function()
+                    return {
+                        style = 'minimal',
+                        split = 'right',
+                    }
+                end,
+                should_enter = false,
+            }
+            require("bolt-server").setup({
+                term_configs = {
+                    compile = bottomsplit,
+                    genapi = bottomsplit,
+                    test = rightsplit,
+                }
             })
         end,
     },
