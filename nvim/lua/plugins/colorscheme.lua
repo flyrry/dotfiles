fc30081c24e68c1b36485282272e3a9e5035c4de
Sergei Mironenko <sergei.mironenko@bolt.eu>
2026-01-30 16:33:25 +0200

    new neovim theme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -29,6 +29,7 @@ return {
         "folke/tokyonight.nvim",
         name = "tokyonight",
         priority = 1000,
+        lazy = true,
         config = function()
             require('tokyonight').load({
                 style = 'storm',
@@ -51,5 +52,112 @@ return {
             vim.cmd.colorscheme('catppuccin')
         end,
     },
-    -- 'deparr/tairiki.nvim',
+    {
+        "EdenEast/nightfox.nvim",
+        lazy = true,
+        priority = 1000,
+        config = function()
+            vim.cmd("colorscheme nordfox") -- duskfox is an option
+        end,
+    },
+    {
+        'AlexvZyl/nordic.nvim',
+        lazy = true,
+        priority = 1000,
+        config = function()
+            vim.cmd("colorscheme nordic")
+        end
+    },
+    {
+        'rmehri01/onenord.nvim',
+        name = 'onenord',
+        lazy = false,
+        priority = 1000,
+        config = function()
+            vim.cmd("colorscheme onenord")
+        end,
+    },
+    {
+        'shaunsingh/nord.nvim',
+        name = 'nord',
+        lazy = true,
+        priority = 1000,
+        config = function()
+            vim.cmd("colorscheme nord")
+        end,
+    },
+    {
+        'mhartington/oceanic-next',
+        name = 'oceanicnext',
+        lazy = true,
+        priority = 1000,
+        config = function()
+            vim.cmd("colorscheme oceanicnext")
+        end,
+    },
+    {
+        'ribru17/bamboo.nvim',
+        lazy = true,
+        priority = 1000,
+        config = function()
+            require('bamboo').setup {
+                -- optional configuration here
+            }
+            require('bamboo').load()
+        end,
+    },
+    {
+        'sainnhe/edge',
+        lazy = true,
+        priority = 1000,
+        config = function()
+            vim.cmd("colorscheme edge")
+        end,
+    },
+    -- -- poor contrast
+    -- {
+    --     "xeind/nightingale.nvim",
+    --     lazy = true,
+    --     priority = 1000,
+    --     config = function()
+    --         require("nightingale").setup({
+    --             transparent = false, -- set to true for transparent background
+    --         })
+    --         vim.cmd("colorscheme nightingale")
+    --     end,
+    -- },
+    -- {
+    --     'rebelot/kanagawa.nvim',
+    --     name = 'kanagawa',
+    --     lazy = true,
+    --     priority = 1000,
+    --     config = function()
+    --         vim.cmd.colorscheme('kanagawa-wave')
+    --     end,
+    -- },
+    -- {
+    --     "vague-theme/vague.nvim",
+    --     lazy = true,
+    --     priority = 1000,
+    --     config = function()
+    --         vim.cmd("colorscheme vague")
+    --     end
+    -- },
+    -- {
+    --     'rose-pine/neovim',
+    --     name = 'rose-pine',
+    --     lazy = true,
+    --     priority = 1000,
+    --     config = function()
+    --         vim.cmd.colorscheme('rose-pine-moon')
+    --     end,
+    -- },
+    -- {
+    --     'olimorris/onedarkpro.nvim',
+    --     lazy = true,
+    --     priority = 1000,
+    --     config = function()
+    --         vim.cmd("colorscheme onedark")
+    --     end,
+    -- },
 }
