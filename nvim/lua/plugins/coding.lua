b63c62ed3cb393ee9262447ff7f0f9147eaedcf4
Sergei Mironenko <sergei.mironenko@bolt.eu>
2026-04-15 20:43:16 +0300

    trying neogit

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -85,6 +85,24 @@ return {
         end
     },
     {
+        "NeogitOrg/neogit",
+        lazy = true,
+        dependencies = {
+            "nvim-lua/plenary.nvim", -- required
+
+            -- Only one of these is needed.
+            -- "sindrets/diffview.nvim", -- optional
+            "esmuellert/codediff.nvim", -- optional
+
+            -- For a custom log pager
+            -- "m00qek/baleia.nvim", -- optional
+        },
+        cmd = "Neogit",
+        keys = {
+            { "<leader>gg", "<cmd>Neogit<cr>", desc = "Show Neogit UI" }
+        }
+    },
+    {
         'bolteu/bolt-server.nvim',
         dependencies = {
             'nvim-treesitter/nvim-treesitter',
